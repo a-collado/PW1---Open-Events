10e4fcd0ef643a84d537425a1378d973cfcdd410
a-collado <57129654+a-collado@users.noreply.github.com>
2022-09-30 16:33:10 +0200

    Todo listo para empezar a trabajar

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Hellow_World from '../components/HelloWorld.vue'
+
 
 const routes = [
     {
         path: '/',
-        name: '',
-        component: 
+        name: 'Hellow_World',
+        component: Hellow_World
     }
 ]
 
