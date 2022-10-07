6913c2e8cbb84537f315ec8d157a53821ba33657
a-collado <a-collado>
2022-10-07 18:54:45 +0200

    Avances en la pagina del perfil

--- src/components/Perfil.vue
+++ src/components/Perfil.vue
@@ -4,6 +4,11 @@
     src="https://cnnespanol.cnn.com/wp-content/uploads/2022/08/220731233929-hyperion-tree-full-169.jpg?quality=100&strip=info" alt="Profile">
   <img class="profilePic"
   src="../assets/images/profilepic.webp" alt="Avatar">
- </div>
+  <div class="profileButtons">
+    <button>10 Amigos</button>
+    <button>2 Eventos</button>
+  </div>
+</div>
+
 
 </template>
