7f325d671ec91cd64a7a9deb77a8da353f589c3f
a-collado <a-collado>
2022-10-07 17:04:48 +0200

    Añadida imagen de perfil

--- src/components/Perfil.vue
+++ src/components/Perfil.vue
@@ -1,8 +1,9 @@
 <template>
  <div class="profile_header">
   <img class="landscape"
-    src="https://cnnespanol.cnn.com/wp-content/uploads/2022/08/220731233929-hyperion-tree-full-169.jpg?quality=100&strip=info">
-       
+    src="https://cnnespanol.cnn.com/wp-content/uploads/2022/08/220731233929-hyperion-tree-full-169.jpg?quality=100&strip=info" alt="Profile">
+  <img class="profilePic"
+  src="../assets/images/profilepic.webp" alt="Avatar">
  </div>
 
 </template>
