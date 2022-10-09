629e815614bfa6e07070924638f59b0d81fe02ab
Laura_fpl_gmail <laura.pascu.lp@gmail.com>
2022-10-09 14:20:46 +0200

    Organitzar fitxer estils i inici vista event

--- src/components/Perfil.vue
+++ src/components/Perfil.vue
@@ -1,13 +1,18 @@
 <template>
  <div class="profile_header">
-  <img class="landscape"
-    src="https://cnnespanol.cnn.com/wp-content/uploads/2022/08/220731233929-hyperion-tree-full-169.jpg?quality=100&strip=info" alt="Profile">
-  <img class="profilePic"
-  src="../assets/images/profilepic.webp" alt="Avatar">
+  <img class="landscape" src="https://cnnespanol.cnn.com/wp-content/uploads/2022/08/220731233929-hyperion-tree-full-169.jpg?quality=100&strip=info" alt="Profile">
+  <img class="profilePic" src="../assets/images/profilepic.webp" alt="Avatar">
+  
   <div class="profileButtons">
     <button>10 Amigos</button>
     <button>2 Eventos</button>
   </div>
+
+</div>
+
+<div class="basic_event">
+  <img class="event_img" src="../assets/images/events/80_party_event.jpg" alt="image of the event">
+
 </div>
 
 
