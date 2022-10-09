2f03d9a16927930ca53ca94c7403d9b5e689b83b
Laura_fpl_gmail <laura.pascu.lp@gmail.com>
2022-10-09 17:12:16 +0200

    Base event

--- src/components/Perfil.vue
+++ src/components/Perfil.vue
@@ -1,19 +1,40 @@
 <template>
- <div class="profile_header">
-  <img class="landscape" src="https://cnnespanol.cnn.com/wp-content/uploads/2022/08/220731233929-hyperion-tree-full-169.jpg?quality=100&strip=info" alt="Profile">
-  <img class="profilePic" src="../assets/images/profilepic.webp" alt="Avatar">
+
+  <div>
+    <div class="profile_header">
+      <img class="landscape" src="https://cnnespanol.cnn.com/wp-content/uploads/2022/08/220731233929-hyperion-tree-full-169.jpg?quality=100&strip=info" alt="Profile">
+      <img class="profilePic" src="../assets/images/profilepic.webp" alt="Avatar">
+      
+      <div class="profileButtons">
+        <button>10 Amigos</button>
+        <button>2 Eventos</button>
+      </div>
+
+      <!-- User info-->
+      <div> 
+        <h1>Nombre</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit facilisis erat eu pulvinar. Nam in tincidunt dolor. Fusce non rhoncus ligula. Proin gravida ex a nisi mollis, venenatis gravida sapien aliquet. Nam sed lectus magna.</p>
+      </div>
+
+    </div>
+
+    
+    <div class="basic_event">
+      <img class="event_img" src="../assets/images/events/80_party_event.jpg" alt="image of the event">
+      
+      <div class="footer_basic_event"> 
+        <P>bla</P>
+      </div>
+    </div>
+
   
-  <div class="profileButtons">
-    <button>10 Amigos</button>
-    <button>2 Eventos</button>
-  </div>
 
 </div>
 
-<div class="basic_event">
-  <img class="event_img" src="../assets/images/events/80_party_event.jpg" alt="image of the event">
 
-</div>
+  
+
+
 
 
 </template>
