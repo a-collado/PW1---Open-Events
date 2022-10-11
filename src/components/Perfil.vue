348213aead32dcb692661473f46c74a3bf6350e0
a-collado <a-collado>
2022-10-11 13:39:35 +0200

    Añadidos amigos en perfil PC

--- src/components/Perfil.vue
+++ src/components/Perfil.vue
@@ -8,30 +8,75 @@
         <button>10 Amigos</button>
         <button>2 Eventos</button>
       </div>
+    </div>
 
+    <div class="vl"></div>
+
+    <div class="profile_friends">
+      <h1>Amigos (5)</h1>
+      <div>
+        <img src="../assets/images/profilepic.webp" alt="profile pic">
+          <div>
+            <h3>Amigo1</h3>
+            <h5>@amigo1</h5>
+        </div>  
+      </div>
+      <hr>
+      <div>
+        <img src="../assets/images/profilepic.webp" alt="profile pic">
+          <div>
+            <h3>Amigo1</h3>
+            <h5>@amigo1</h5>
+        </div>  
+      </div>
+      <hr>
+      <div>
+        <img src="../assets/images/profilepic.webp" alt="profile pic">
+          <div>
+            <h3>Amigo1</h3>
+            <h5>@amigo1</h5>
+        </div>  
+      </div>
+      <hr>
+      <div>
+        <img src="../assets/images/profilepic.webp" alt="profile pic">
+          <div>
+            <h3>Amigo1</h3>
+            <h5>@amigo1</h5>
+        </div>  
+      </div>
+      <hr>
+      <div>
+        <img src="../assets/images/profilepic.webp" alt="profile pic">
+          <div>
+            <h3>Amigo1</h3>
+            <h5>@amigo1</h5>
+        </div>  
+      </div>
+      <hr>
       
-    
     </div>
 
     <!-- User info-->
-
 
     <div class="profile_info"> 
         <article>
         <h1>Nombre</h1>
         <h4>@nombre2</h4>
         </article>
+        <div>
         <h4>Barcelona, España</h4>
-        <div class="user_rating">
-          <img src="../assets/images/icons/star_b.png" alt="1 estrella">
-          <img src="../assets/images/icons/star_b.png" alt="2 estrellas">
-          <img src="../assets/images/icons/star_b.png" alt="3 estrellas">
-          <img src="../assets/images/icons/star_b.png" alt="4 estrellas">
-          <img src="../assets/images/icons/star_b.png" alt="5 estrellas">
-          <h5>5</h5>
+          <div class="user_rating">
+            <img src="../assets/images/icons/star_b.png" alt="1 estrella">
+            <img src="../assets/images/icons/star_b.png" alt="2 estrellas">
+            <img src="../assets/images/icons/star_b.png" alt="3 estrellas">
+            <img src="../assets/images/icons/star_b.png" alt="4 estrellas">
+            <img src="../assets/images/icons/star_b.png" alt="5 estrellas">
+            <h5>5</h5>
+          </div>
         </div>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit facilisis erat eu pulvinar. Nam in tincidunt dolor. Fusce non rhoncus ligula. Proin gravida ex a nisi mollis, venenatis gravida sapien aliquet. Nam sed lectus magna.</p>
-        <div>
+        <div class="button_flex">
           <button>Editar perfil</button>
           <button-icon><img class="icon" src="../assets/images/icons/config.png" alt="profile configuration">
           </button-icon>
