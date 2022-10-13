6a86a8c9c11e95c5b62ca800196f8e645470e288
Guillem Rosello <guillemroch@gmail.com>
2022-10-13 17:06:49 +0200

    Register

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="background">
         <div class="Sign_in_box">
-            <h1>Sign Up</h1>
-            <input class="sign_in_input" type="text" placeholder="Username">
-            <input class="sign_in_input" type="password" placeholder="Password">
-            <router-link to="/" class="sign_in_button"><p>Login</p></router-link>
-            <div class="register_here">
-                <p>Not Registered?</p>
-                <router-link to="/r" id="button">Register here</router-link>
-            </div>
+            <h1>Register</h1>
+            <!--<div class="register_box"> -->  
+                <input class="sign_in_input" type="text" placeholder="Name">
+                <input class="sign_in_input" type="text" placeholder="Last Name">
+                <input class="sign_in_input" type="date" placeholder="Date of Birth">
+                <input class="sign_in_input" type="text" placeholder="Email">
+                <input class="sign_in_input" type="password" placeholder="Password">
+                <input class="sign_in_input" type="password" placeholder="Confirm password">
+                <router-link to="/" class="sign_in_button"><p>Register</p></router-link>
+            
         </div>
     </div>
 </template>
