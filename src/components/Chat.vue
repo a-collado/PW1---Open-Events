5802406fef0a1e7d2dd73ff0c6ab422e93a69c40
a-collado <57129654+a-collado@users.noreply.github.com>
2022-10-18 14:27:46 +0200

    Chat hecho (mobil)

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -11,6 +11,34 @@
         <h5 class="yellow">Ocupado</h5>
     </div>
 </article>
+<main>
+    <div class="chat">
+        <div class="message received">
+            <p>Ut ex turpis, cursus sit amet odio elementum.</p>
+            <h4>15:25</h4>
+        </div>
+        <div class="message send">
+            <p>Quisque elementum justo sem</p>
+            <img class="icon" src="../assets/images/icons/read_g.png" alt="leido">
+            <h4>16:00</h4>
+        </div>
+        <div class="message received">
+            <p>Ut ex turpis, cursus sit amet odio elementum</p>
+            <h4>15:25</h4>
+        </div>
+        <div class="message send">
+            <p>Quisque elementum justo sem</p>
+            <img class="icon" src="../assets/images/icons/check_g.png" alt="leido">
+            <h4>18:20</h4>
+        </div>
+    </div>
+</main>
+<footer class="flex_row_wrap">
+    <input class="searchbar" type="text" placeholder="Escribe aqui...">
+    <ellipse>
+        <img class="icon" src="../assets/images/icons/send.png" alt="leido">
+    </ellipse>
+</footer>
 
 </template>
 
@@ -35,6 +63,26 @@
     justify-content: space-between;
 }
 
+footer.flex_row_wrap{
+    position: fixed;
+    justify-content: space-evenly;
+    padding-top: 20px;
+    padding-bottom: 25px;
+    padding-left: 5%;
+    bottom: 0px;
+    background: linear-gradient(180deg, rgba(0, 0, 0, 0.03) 68.23%, rgba(0, 0, 0, 0.11) 100%);
+    box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+
+footer .searchbar{
+    width: 30vh;
+    background: rgba(0, 0, 0, 0.06);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 19px;
+    outline: none;
+    border: none;
+}
+
 .profile_pic_message{
     width: 61px;
     height: 61px;
@@ -43,9 +91,78 @@
     border-radius: 50%;
 }
 
+.chat{
+    display: flex;
+    flex-direction: column;
+    margin: 25px;
+    height: 65vw;
+    overflow: auto;
+}
+
+.message{
+    display: flex;
+    flex-wrap: nowrap;
+    width: fit-content;
+    border-radius: 10px;
+    margin-bottom: 10px;
+}
+
+.message.send{
+    background: linear-gradient(180deg, #25585D 0%, rgba(61, 178, 190, 0.79) 0.01%, rgba(59, 225, 240, 0.48) 100%);
+    align-self: flex-end;
+}
+
+.message.received{
+    background: linear-gradient(180deg, rgba(0, 0, 0, 0.06) 0%, rgba(0, 0, 0, 0.0222) 100%);
+    align-self: flex-start;
+}
+
+ellipse{
+    padding: 8px;
+    border-radius: 50%;
+    background: #E26ABA;
+    
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 10px;
+    line-height: 12px;
+    color: #FFFFFF;
+
+    background: radial-gradient(50% 50% at 50% 50%, #E26ABA 0%, rgba(226, 106, 186, 0.48) 15.62%, #E26ABA 80.21%);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    }
+
 .icon{
     width: 35px;
     height: 35px;
+}
+
+.message .icon{
+    width: 17px;
+    height: 17px;
+    align-self: flex-end;
+    margin-bottom: 12px;
+    margin-left: 10px;
+    margin-right: 5px;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+}
+
+ellipse img.icon{
+    width: 30px;
+    height: 29px;
+}
+
+p{
+    margin: 17px;
+    margin-right: 3px;
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 17px;
+    width: 25vh;
+    color: #000000;
 }
 
 h4{
@@ -56,6 +173,28 @@ h4{
     line-height: 15px;
     margin: 0px;
     margin-left: 12px;
+}
+
+.message h4{
+    position: relative;
+    margin: 0px;
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 300;
+    font-size: 10px;
+    line-height: 12px;
+    letter-spacing: -0.11em;
+    color: #ABABAB;
+    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    align-self: flex-end;
+    margin-right: 15px;
+    margin-bottom: 15px;
+    margin-left: 10px;
+}
+
+.message.send h4{
+    margin-left: 0px;
+    color: #ffffff;
 }
 
 h5{
