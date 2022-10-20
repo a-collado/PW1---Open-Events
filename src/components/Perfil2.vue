399abe84fcec163377377672c65181b0aef6b5a3
Laura_fpl_gmail <laura.pascu.lp@gmail.com>
2022-10-20 19:16:22 +0200

    TimelineEvents2

--- src/components/Perfil2.vue
+++ src/components/Perfil2.vue
@@ -129,66 +129,69 @@
         </div> <!--Tanquem div centered_horitzontal-->
 
         <div class="centered_horitzontal">
+          
           <div class="background_white_opac">
             
             <h2 class="blue_big" style="margin-left:20px">Marzo 2023</h2>
 
-            <div class="timeline_event">
-              <img class="event_img" src="../assets/images/events/80_party_event.jpg" alt="image of the event">
-              
-              <div class="footer_timelineEvent"> 
-                <div>
-                  <h2 class="blue_big">Fiesta de los 80</h2>
-                  <div class="flex_row_wrap">
-                    <img class="icon" src="../assets/images/icons/comment.png" alt="icon">
-                    <p class="pink_small_bold">134 comentarios</p>
-                  </div>
-                  <p class="blue_small_bold">Un 96% de usuarios han comentado más que tu</p>
-                </div>
-
-                <div class="column"> 
-                  <div class="flex_row_wrap">
-                    <img class="icon" src="../assets/images/icons/schedule.png" alt="icon">
-                    <p class="blue_small_bold">09/11/2021<br>20:00</p>
+            <div class="flex_row_wrap">
+              <div class="timeline_event">
+                <img class="event_img" src="../assets/images/events/80_party_event.jpg" alt="image of the event">
+                
+                <div class="footer_timelineEvent"> 
+                  <div>
+                    <h2 class="blue_big">Fiesta de los 80</h2>
+                    <div class="flex_row_wrap">
+                      <img class="icon" src="../assets/images/icons/comment.png" alt="icon">
+                      <p class="pink_small_bold">134 comentarios</p>
+                    </div>
+                    <p class="blue_small_bold">Un 96% de usuarios han comentado más que tu</p>
                   </div>
 
-                  <div class="flex_row_wrap">
-                    <img class="icon" src="../assets/images/icons/maps.png" alt="icon">
-                    <p class="blue_small_bold">Barcelona</p>
-                  </div>
-                </div>
+                  <div class="column"> 
+                    <div class="flex_row_wrap">
+                      <img class="icon" src="../assets/images/icons/schedule.png" alt="icon">
+                      <p class="blue_small_bold">09/11/2021<br>20:00</p>
+                    </div>
 
-              </div><!--Footer del event-->
-            </div> <!--Tanquem div del event-->
-
-
-            <div class="timeline_event">
-              <img class="event_img" src="../assets/images/events/80_party_event.jpg" alt="image of the event">
-              
-              <div class="footer_timelineEvent"> 
-                <div>
-                  <h2 class="blue_big">Fiesta de los 80</h2>
-                  <div class="flex_row_wrap">
-                    <img class="icon" src="../assets/images/icons/comment.png" alt="icon">
-                    <p class="pink_small_bold">134 comentarios</p>
-                  </div>
-                  <p class="blue_small_bold">Un 96% de usuarios han comentado más que tu</p>
-                </div>
-
-                <div class="column"> 
-                  <div class="flex_row_wrap">
-                    <img class="icon" src="../assets/images/icons/schedule.png" alt="icon">
-                    <p class="blue_small_bold">09/11/2021<br>20:00</p>
+                    <div class="flex_row_wrap">
+                      <img class="icon" src="../assets/images/icons/maps.png" alt="icon">
+                      <p class="blue_small_bold">Barcelona</p>
+                    </div>
                   </div>
 
-                  <div class="flex_row_wrap">
-                    <img class="icon" src="../assets/images/icons/maps.png" alt="icon">
-                    <p class="blue_small_bold">Barcelona</p>
-                  </div>
-                </div>
+                </div><!--Footer del event-->
+              </div> <!--Tanquem div del event-->
 
-              </div><!--Footer del event-->
-            </div> <!--Tanquem div del event-->
+
+              <div class="timeline_event">
+                <img class="event_img" src="../assets/images/events/80_party_event.jpg" alt="image of the event">
+                
+                <div class="footer_timelineEvent"> 
+                  <div>
+                    <h2 class="blue_big">Fiesta de los 80</h2>
+                    <div class="flex_row_wrap">
+                      <img class="icon" src="../assets/images/icons/comment.png" alt="icon">
+                      <p class="pink_small_bold">134 comentarios</p>
+                    </div>
+                    <p class="blue_small_bold">Un 96% de usuarios han comentado más que tu</p>
+                  </div>
+
+                  <div class="column"> 
+                    <div class="flex_row_wrap">
+                      <img class="icon" src="../assets/images/icons/schedule.png" alt="icon">
+                      <p class="blue_small_bold">09/11/2021<br>20:00</p>
+                    </div>
+
+                    <div class="flex_row_wrap">
+                      <img class="icon" src="../assets/images/icons/maps.png" alt="icon">
+                      <p class="blue_small_bold">Barcelona</p>
+                    </div>
+                  </div>
+
+                </div><!--Footer del event-->
+              </div> <!--Tanquem div del event-->
+            </div> <!--Tanquem div dels events-->
 
 
           </div>  <!--Tanquem div de tots els events -->
