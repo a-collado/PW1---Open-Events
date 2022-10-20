50e4a775e1d526c593721abe5fbecaede6f0cc7f
annafegi <anna.fg@students.salle.url.edu>
2022-10-20 19:15:44 +0200

    updates event vue

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -5,25 +5,33 @@
     </div>
 
     <div class="event_descrip_box">
-        <h1>Descripción</h1>
-        <div class="punctuation">
-            <h5>5</h5>
-            <img class="stars" src="../assets/images/icons/star_b.png" alt="1 estrella">
-            <img class="stars" src="../assets/images/icons/star_b.png" alt="2 estrellas">
-            <img class="stars" src="../assets/images/icons/star_b.png" alt="3 estrellas">
-            <img class="stars" src="../assets/images/icons/star_b.png" alt="4 estrellas">
-            <img class="stars" src="../assets/images/icons/star_b.png" alt="5 estrellas">
+        <div class="event_descrip_puntuat_box">
+            <h1>Descripción</h1>
+            <div class="puntuation">
+                <h5>5</h5>
+                <img class="stars" src="../assets/images/icons/star_b.png" alt="1 estrella">
+                <img class="stars" src="../assets/images/icons/star_b.png" alt="2 estrellas">
+                <img class="stars" src="../assets/images/icons/star_b.png" alt="3 estrellas">
+                <img class="stars" src="../assets/images/icons/star_b.png" alt="4 estrellas">
+                <img class="stars" src="../assets/images/icons/star_b.png" alt="5 estrellas">
+            </div>
         </div>
         <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit facilisis erat eu pulvinar. Nam in tincidunt dolor. Fusce non rhoncus ligula. Proin gravida ex a nisi mollis, venenatis gravida sapien aliquet. Nam sed lectus magna.</h5>
-        <button>Participar</button>
-        <button>Compartir</button>
+        <div class="event_buttons_box">
+            <button>Participar</button>
+            <button>Compartir</button>
+        </div>
     </div>
 
     <div class="event_info_box">
-        <img class="icon" src="../assets/images/icons/schedule.png" alt="icon">
-        <h5>25 Diciembre 2022 (20h00 - 24h00)</h5>
-        <h5>Calle 4 Camins 256, 08019, Barcelona</h5>
-        <img class="icon" src="../assets/images/icons/gglMapsButton.png" alt="icon">
+        <div class="event_date_box">
+            <img class="icon" src="../assets/images/icons/schedule.png" alt="icon">
+            <h5>25 Diciembre 2022 (20h00 - 24h00)</h5>
+        </div>
+        <div class="event_location_box">
+            <h5>Calle 4 Camins 256, 08019, Barcelona</h5>
+            <img class="icon" src="../assets/images/icons/gglMapsButton.png" alt="icon">
+        </div>
     </div>
 
     <div class="event_participantes_box">
@@ -33,18 +41,22 @@
     </div>
 
     <div class="event_resenas_box">
-        <h1>Reseñas del Evento</h1>
-        <button-icon><img class="icon" src="../assets/images/icons/up-down.png" alt="filter"></button-icon>
+        <div class="event_resenas_header_box">
+            <h1>Reseñas del Evento</h1>
+            <button-icon><img class="icon" src="../assets/images/icons/up-down.png" alt="filter"></button-icon>
+        </div>
         <div class="resenas_box">
             <div class="resena_box">
-                <h5>Nombre Apellido</h5>
-                <div class="punctuation">
-                    <h5>5</h5>
-                    <img class="stars" src="../assets/images/icons/star_b.png" alt="1 estrella">
-                    <img class="stars" src="../assets/images/icons/star_b.png" alt="2 estrellas">
-                    <img class="stars" src="../assets/images/icons/star_b.png" alt="3 estrellas">
-                    <img class="stars" src="../assets/images/icons/star_b.png" alt="4 estrellas">
-                    <img class="stars" src="../assets/images/icons/star_b.png" alt="5 estrellas">
+                <div class="resena_persona_box">
+                    <h5>Nombre Apellido</h5>
+                    <div class="punctuation">
+                        <h5>5</h5>
+                        <img class="stars" src="../assets/images/icons/star_b.png" alt="1 estrella">
+                        <img class="stars" src="../assets/images/icons/star_b.png" alt="2 estrellas">
+                        <img class="stars" src="../assets/images/icons/star_b.png" alt="3 estrellas">
+                        <img class="stars" src="../assets/images/icons/star_b.png" alt="4 estrellas">
+                        <img class="stars" src="../assets/images/icons/star_b.png" alt="5 estrellas">
+                    </div>
                 </div>
                 <p class="grey_normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit facilisis erat eu pulvinar. Nam in tincidunt dolor. Fusce non rhoncus ligula. Proin gravida ex a nisi mollis, venenatis gravida sapien aliquet. Nam sed lectus magna.</p>
             </div>
@@ -57,6 +69,19 @@
                     <img class="stars" src="../assets/images/icons/star_b.png" alt="3 estrellas">
                     <img class="stars" src="../assets/images/icons/star_b.png" alt="4 estrellas">
                     <img class="stars" src="../assets/images/icons/star_b.png" alt="5 estrellas">
+                </div>
+                <p class="grey_normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit facilisis erat eu pulvinar. Nam in tincidunt dolor. Fusce non rhoncus ligula. Proin gravida ex a nisi mollis, venenatis gravida sapien aliquet. Nam sed lectus magna.</p>
+            </div><div class="resena_box">
+                <div class="resena_persona_box">
+                    <h5>Nombre Apellido</h5>
+                    <div class="punctuation">
+                        <h5>5</h5>
+                        <img class="stars" src="../assets/images/icons/star_b.png" alt="1 estrella">
+                        <img class="stars" src="../assets/images/icons/star_b.png" alt="2 estrellas">
+                        <img class="stars" src="../assets/images/icons/star_b.png" alt="3 estrellas">
+                        <img class="stars" src="../assets/images/icons/star_b.png" alt="4 estrellas">
+                        <img class="stars" src="../assets/images/icons/star_b.png" alt="5 estrellas">
+                    </div>
                 </div>
                 <p class="grey_normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit facilisis erat eu pulvinar. Nam in tincidunt dolor. Fusce non rhoncus ligula. Proin gravida ex a nisi mollis, venenatis gravida sapien aliquet. Nam sed lectus magna.</p>
             </div>
