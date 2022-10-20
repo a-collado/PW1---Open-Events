49524d5c12ccaa6f95bbb8034927e7e84840c890
Guillem Rosello <guillemroch@gmail.com>
2022-10-20 23:01:19 +0200

    Home cositas

---
+++ src/App-DESKTOP-MP83VRL.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Perfil from './components/Perfil.vue'
+import Main from './components/Main.vue'
+import Welcome from './components/Welcome.vue'
+</script>
+
+<template>
+  <div class="header">
+    <h2>Hola</h2>
+    <input class="searchbar" type="text" placeholder="¿Que tipo de evento estas buscando?">
+  </div>
+  <nav class="navbar">
+    <router-link to="/m">Main</router-link>
+    <router-link to="/">Perfil</router-link>
+    <router-link to="/w">Welcome</router-link>
+    <router-link to="/s">Sign in</router-link>
+  </nav>
+
+  <router-view></router-view>
+  
+  </template>
+
+
