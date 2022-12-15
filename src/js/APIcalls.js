0507506d95794976c3295688adefe0bf9655ac48
LauraFPL <102858676+LauraFPL@users.noreply.github.com>
2022-12-15 20:08:54 +0100

    getElementbyId--> canvi a vbinds

--- src/js/APIcalls.js
+++ src/js/APIcalls.js
@@ -75,7 +75,7 @@ export default class ApiCalls{
         return this.fetchPost('http://puigmal.salle.url.edu/api/v2/users/login', user)
         .then(response => response.json())
         .then((body) =>{
-            console.log(this.CORRECT); //! OJO, QUE SURT QUE ÉS UNDEFINED - CAL MIRAR 
+            //console.log(this.CORRECT); //! OJO, QUE SURT QUE ÉS UNDEFINED - CAL MIRAR 
             if (typeof body.Error === 'undefined'){ 
                 window.localStorage.setItem("accessToken", body.accessToken);
                 //console.log(window.localStorage.getItem("accessToken"))
@@ -86,13 +86,18 @@ export default class ApiCalls{
         })
         .then((result) =>{
             
-            this.fetchGetBearerToken('http://puigmal.salle.url.edu/api/v2/users/search?s=' + email)
-            .then(response => response.json())
-            .then((user)=>{
-                //console.log(user);
-                //console.log(user[0].id);
-                window.localStorage.setItem("loggedUser", user[0].id);
-            });
+            if (result == this.correct){
+                this.fetchGetBearerToken('http://puigmal.salle.url.edu/api/v2/users/search?s=' + email)
+                .then(response => response.json())
+                .then((user)=>{
+                    //console.log(user);
+                    //console.log(user[0].id);
+                    window.localStorage.setItem("loggedUser", user[0].id);
+                });
+                return this.CORRECT;
+            }else{
+                return result;
+            }
         });
     }
 
